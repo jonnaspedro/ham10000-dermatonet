440a7668d20602ed4010627d4b3a6ed0784838c6
Vitor <jvjv10112607@gmail.com>
2025-12-08 13:14:21 -0300

    hello world message

--- train_model.py
+++ train_model.py
@@ -1,6 +1,1 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
-
-print("Path to dataset files:", path)
+print("Hello, world!")
